3a36e25b36693788e31849f2d63a1b62a6c467cc
Himali Vaghela <57657231+himalivaghela@users.noreply.github.com>
2025-02-04 06:51:23 +0000

    demo_code

---
+++ demo.py
@@ -0,0 +1,6 @@
+# create multiplication between two numbers
+
+a = 10  # Initialize variable a with value 10
+b = 10  # Initialize variable b with value 10
+
+c = a * b  # Multiply a and b, and assign the result to c
